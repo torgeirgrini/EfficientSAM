33532c7861971ef492467519c5a7a01c1a49d59c
Jon Torgeir Grini <torgeirgrini@gmail.com>
2024-03-17 13:53:52 +0100

    added paths

--- efficient_sam/build_efficient_sam.py
+++ efficient_sam/build_efficient_sam.py
@@ -6,11 +6,12 @@
 
 from .efficient_sam import build_efficient_sam
 
+
 def build_efficient_sam_vitt():
     return build_efficient_sam(
         encoder_patch_embed_dim=192,
         encoder_num_heads=3,
-        checkpoint="/home/torgeir/Documents/mask_matching/EfficientSAM/weights/efficient_sam_vitt.pt",
+        checkpoint="/media/torgeir/LaCie 2TB/mask_matching/EfficientSAM/weights/efficient_sam_vitt.pt",
     ).eval()
 
 
@@ -18,5 +19,5 @@ def build_efficient_sam_vits():
     return build_efficient_sam(
         encoder_patch_embed_dim=384,
         encoder_num_heads=6,
-        checkpoint="/home/torgeir/Documents/mask_matching/EfficientSAM/weights/efficient_sam_vits.pt",
+        checkpoint="/media/torgeir/LaCie 2TB/mask_matching/EfficientSAM/weights/efficient_sam_vits.pt",
     ).eval()
